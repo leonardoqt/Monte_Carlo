94710ca2d9e4a070e9a5bc5f5c5de38c5353b64e
leonardoqt <unsigneddn@gmail.com>
2019-07-27 13:46:21 -0400

    figure out default criterial, period ~ 10*kind*checkpoint, temperature use ene to ene/10000, profile use mixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <vector>
 #include <cmath>
@@ -7,12 +8,13 @@
 
 using namespace std;
 
-double get_ene(vector < vector <double> >& param, vector < vector <double> >& target)
+//Rosenbrock
+double get_ene(vector < vector <double> >& param)
 {
 	double ene = 0;
 	for(size_t t1=0; t1<param.size(); t1++)
-		for(size_t t2=0; t2<param[t1].size(); t2++)
-			ene += (param[t1][t2]-target[t1][t2])*(param[t1][t2]-target[t1][t2]);
+		for(size_t t2=0; t2<param[t1].size()-1; t2++)
+			ene += pow((1-param[t1][t2]),2)+100*pow(param[t1][t2+1]-pow(param[t1][t2],2),2);
 	return ene;
 }
 
@@ -23,43 +25,47 @@ int main()
 	mc mc1;
 	thermo_profile Tem1;
 	int T_max, T_min;
-	int period = 300;
 	int num_kind = 3;
 	int check_point = 30;
+	int period = num_kind*check_point*10;
 	vector <int> num_param;
 	vector <double> lambda;
-	vector < vector <double> > param, target;
+	vector < vector <double> > param;
 	double ene = 0.0;
 	double count = 0.0;
+	double ene0,ene1,ene2;
 	
 	for(size_t t1=0; t1<num_kind; t1++)
 	{
 		num_param.push_back(t1+2);
 		lambda.push_back(1);
 	}
-	target.resize(num_kind);
 	param.resize(num_kind);
 	count = 0.0;
 	for(size_t t1=0; t1<num_kind; t1++)
 		for(size_t t2=0; t2<num_param[t1]; t2++)
 		{
 			count += 1.0;
-			target[t1].push_back(count);
 			param[t1].push_back(0);
 			
 		}
-	ene = get_ene(param,target);
-	T_max = ene/1000;
-	T_min = T_max/1000;
+	ene = get_ene(param);
+	T_max = ene;
+	T_min = ene/10000;
 	
 	mc1.init(num_kind, check_point, num_param, lambda, param, ene);
 	Tem1.init(period,T_max,T_min);
-	for(size_t t1=0; t1<3*period; t1++)
+	for(size_t t1=0; t1<100*period; t1++)
 	{
 		mc1.gen_param_kind(param);
-		ene = get_ene(param,target);
-		Tem1.gen_T(t1,"log");
+		ene = get_ene(param);
+		Tem1.gen_T(t1,"mixed");
 		mc1.evaluate(Tem1,ene);
+		if ((t1+1)%period==0)
+		{
+			mc1.get_param_ene(param,param,ene0,ene1,ene2);
+			cout<<fixed<<setprecision(6)<<setw(10)<<ene0<<setw(10)<<ene1<<setw(10)<<ene2<<endl;
+		}
 	}
 	mc1.print();
 	return 0;
