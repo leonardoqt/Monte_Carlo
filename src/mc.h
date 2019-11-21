060fcb2cd984f3f728ace2925bfd2fdcd36fa0f0
Tian Qiu <tianq@sas.upenn.edu>
2019-11-21 17:18:13 -0500

    remove namespace std

--- src/mc.h
+++ src/mc.h
@@ -4,8 +4,6 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 class thermo_profile
 {
 private:
@@ -15,7 +13,7 @@ private:
 	int iter;
 public:
 	void init(int Period, double T_Max, double T_Min);
-	double gen_T(int Iter,string profile);
+	double gen_T(int Iter,std::string profile);
 	double gen_T(int Iter);
 	double gen_T();
 };
@@ -25,26 +23,26 @@ class mc
 private:
 	int num_kind;
 	int check_point;
-	vector <int> num_param;
-	vector <double> lambda;
-	vector <double> max_param;
-	vector <double> min_param;
-	vector <int> num_run;
-	vector <int> num_accept;
-	vector < vector <double> > pre_param;
-	vector < vector <double> > opt_param;
-	vector < vector <double> > new_param;
+	std::vector <int> num_param;
+	std::vector <double> lambda;
+	std::vector <double> max_param;
+	std::vector <double> min_param;
+	std::vector <int> num_run;
+	std::vector <int> num_accept;
+	std::vector < std::vector <double> > pre_param;
+	std::vector < std::vector <double> > opt_param;
+	std::vector < std::vector <double> > new_param;
 	double pre_ene;
 	double opt_ene;
 	double new_ene;
 	int kind_change, param_change;
 public:
-	void init(int &Num_kind, int Check_point, vector <int> &Num_param, vector <double> &Lambda, vector <double> &Max_param, vector <double> &Min_param, vector < vector <double> > &Ini_param, double Ini_ene);
-	void gen_param(vector < vector <double> >& param);
-	void gen_param_kind(vector < vector <double> >& param);
+	void init(int &Num_kind, int Check_point, std::vector <int> &Num_param, std::vector <double> &Lambda, std::vector <double> &Max_param, std::vector <double> &Min_param, std::vector < std::vector <double> > &Ini_param, double Ini_ene);
+	void gen_param(std::vector < std::vector <double> >& param);
+	void gen_param_kind(std::vector < std::vector <double> >& param);
 	int evaluate(thermo_profile& thermo, double ene);
 	
-	void get_param_ene(vector < vector <double> >& Pre_param, vector < vector <double> >& Opt_param, double& Pre_ene, double& Opt_ene, double& New_ene);
+	void get_param_ene(std::vector < std::vector <double> >& Pre_param, std::vector < std::vector <double> >& Opt_param, double& Pre_ene, double& Opt_ene, double& New_ene);
 	void print();
 	void print_ene();
 };
